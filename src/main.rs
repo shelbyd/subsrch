ce4ceb98cf8c5eecafd94a70978b3fa0d09f5085
Shelby Doolittle <shelby@shelbyd.com>
2017-12-31 01:10:13 -0500

    add command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "subsrch", about = "Find subsets that pass the given test.")]
+enum SubSrch {
+    #[structopt(name = "maximal", about = "Find the largest subset that passes the test.")]
+    Maximal {
+        #[structopt(help = "Test command.")] test_command: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sub_search = SubSrch::from_args();
+    match sub_search {
+        SubSrch::Maximal { test_command: c } => {
+            println!("{}", c);
+        }
+    }
 }
