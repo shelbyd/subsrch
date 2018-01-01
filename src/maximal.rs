2eea5b4e7003ca1e077d657d45ccfe3e5cdd5303
Shelby Doolittle <shelby@shelbyd.com>
2018-01-01 18:41:00 -0500

    reimplement maximal range

--- src/maximal.rs
+++ src/maximal.rs
@@ -10,9 +10,9 @@ where
     let mut reject_range = RejectRange::new(full.len());
     loop {
         match reject_range.next() {
-            Done(o) => return o.map(|indices| full.reject_indices(&indices)),
+            Done(o) => return o.map(|indices| full.select_indices(&indices)),
             RunTest(indices) => {
-                if test(&full.clone().reject_indices(&indices)) {
+                if test(&full.clone().select_indices(&indices)) {
                     reject_range.test_passed(indices);
                 } else {
                     reject_range.test_failed(indices);
@@ -41,7 +41,7 @@ impl RejectRange {
         self.passed = Some(match self.passed.take() {
             None => included_indices,
             Some(p) => {
-                if included_indices.is_subset(&p) {
+                if included_indices.is_superset(&p) {
                     included_indices
                 } else {
                     p
@@ -49,7 +49,7 @@ impl RejectRange {
             }
         });
         let passed = self.passed.as_ref().unwrap();
-        self.failures.retain(|f| f.is_subset(passed));
+        self.failures.retain(|f| f.is_superset(passed));
     }
 
     fn test_failed(&mut self, included_indices: Indices) {
@@ -61,30 +61,34 @@ impl RejectRange {
             (Some(passed), _) => self.next_indices()
                 .map(RunTest)
                 .unwrap_or_else(|| Done(Some(passed.clone()))),
-            (None, 0) => RunTest((0..self.initial_len).collect()),
+            (None, 0) => RunTest(set![]),
             (None, _) => Done(None),
         }
     }
 
     fn next_indices(&self) -> Option<Indices> {
         let passed = self.passed.as_ref().unwrap();
-        let past_rejects: Vec<_> = once(set![])
-            .chain(self.failures.iter().map(|f| passed - f))
+        let past_includes: Vec<_> = once(set![])
+            .chain(self.failures.iter().map(|f| f - passed))
             .collect();
-        let potential_rejects = once(passed)
-            .chain(past_rejects.iter())
-            .flat_map(|reject| {
-                let mut first: Vec<_> = reject.iter().cloned().collect();
+
+        let all_elements: HashSet<_> = (0..self.initial_len).collect();
+        let unpassed = &all_elements - passed;
+
+        let potential_includes = once(&unpassed)
+            .chain(past_includes.iter())
+            .flat_map(|include| {
+                let mut first: Vec<_> = include.iter().cloned().collect();
                 first.sort();
                 let mid = first.len() / 2;
                 let second = first.split_off(mid);
                 vec![first, second]
             })
             .map(|vec| vec.into_iter().collect::<HashSet<_>>());
-        potential_rejects
-            .filter(|r| !past_rejects.contains(r))
+        potential_includes
+            .filter(|i| !past_includes.contains(i))
             .next()
-            .map(|next_reject| passed - &next_reject)
+            .map(|next_include| passed | &next_include)
     }
 }
 
@@ -173,90 +177,75 @@ mod tests {
         }
 
         #[test]
-        fn single_element_is_test_all() {
+        fn single_element_is_test_empty() {
             let range = RejectRange::new(1);
 
-            assert_eq!(range.next(), RunTest(set![0]));
-        }
-
-        #[test]
-        fn single_element_failed_test_is_none() {
-            let mut range = RejectRange::new(1);
-
-            range.test_failed(set![0]);
-            assert_eq!(range.next(), Done(None));
-        }
-
-        #[test]
-        fn single_element_passed_test_is_test_empty() {
-            let mut range = RejectRange::new(1);
-
-            range.test_passed(set![0]);
             assert_eq!(range.next(), RunTest(set![]));
         }
 
         #[test]
-        fn single_element_required_is_done() {
-            let mut range = RejectRange::new(1);
-
-            range.test_passed(set![0]);
-            range.test_failed(set![]);
-            assert_eq!(range.next(), Done(Some(set![0])));
-        }
-
-        #[test]
-        fn empty_passed_with_one_element_is_done() {
+        fn single_element_failed_is_empty_set() {
             let mut range = RejectRange::new(1);
 
             range.test_passed(set![]);
+            range.test_failed(set![0]);
             assert_eq!(range.next(), Done(Some(set![])));
+        }
+
+        #[test]
+        fn single_element_passed_test_is_done() {
+            let mut range = RejectRange::new(1);
+
+            range.test_passed(set![0]);
+            assert_eq!(range.next(), Done(Some(set![0])));
         }
 
         #[test]
         fn two_elements_failed() {
             let mut range = RejectRange::new(2);
 
-            range.test_failed(set![0, 1]);
+            range.test_failed(set![]);
             assert_eq!(range.next(), Done(None));
         }
 
         #[test]
-        fn two_elements_required() {
+        fn two_elements_only_one() {
             let mut range = RejectRange::new(2);
 
-            range.test_passed(set![0, 1]);
-            range.test_failed(set![1]);
-            range.test_failed(set![0]);
-            assert_eq!(range.next(), Done(Some(set![0, 1])));
+            range.test_passed(set![0]);
+            range.test_failed(set![0, 1]);
+            assert_eq!(range.next(), Done(Some(set![0])));
         }
 
         #[test]
         fn two_elements_first_required() {
             let mut range = RejectRange::new(2);
 
-            range.test_passed(set![0, 1]);
+            range.test_passed(set![]);
+            range.test_failed(set![0, 1]);
             range.test_failed(set![1]);
             assert_eq!(range.next(), RunTest(set![0]));
         }
 
         #[test]
-        fn ignores_larger_passes() {
+        fn ignores_smaller_passes() {
             let mut range = RejectRange::new(2);
 
-            range.test_passed(set![1]);
             range.test_passed(set![0, 1]);
-            assert_eq!(range.next(), RunTest(set![]));
+            range.test_passed(set![1]);
+            assert_eq!(range.next(), Done(Some(set![0, 1])));
         }
 
         #[test]
-        fn four_elements_requires_middle_two() {
+        fn four_elements_rejects_middle_two() {
             let mut range = RejectRange::new(4);
 
-            range.test_passed(set![0, 1, 2, 3]);
+            range.test_passed(set![]);
+            range.test_failed(set![0, 1, 2, 3]);
             range.test_failed(set![0, 1]);
             range.test_failed(set![2, 3]);
             match range.next() {
-                RunTest(v) => assert!(v.len() == 3, "{:?} should have length 3", v),
+                RunTest(v) => assert!(v.len() == 1, "{:?} should have length 1", v),
                 _ => assert!(false),
             }
         }
